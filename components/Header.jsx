8c24426351184214c20dbf1f27dd23366675ecea
Jameson Chen <[jamesonychan@gmail.com]>
2021-12-11 22:09:50 -0500

    instead of using debounce, use ref to resolve fetching limits issue on search component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,6 @@
-import React, { useMemo, useEffect } from "react";
+import React, { useRef } from "react";
 import { useRecoilState } from "recoil";
 import { searchState } from "../atoms/SearchAtom";
-import { debounce } from "lodash";
 import Image from "next/image";
 import Mask from "../images/istockphoto-1210279557-1024x1024.png";
 import {SearchIcon} from '@heroicons/react/outline'
@@ -9,32 +8,26 @@ import {SearchIcon} from '@heroicons/react/outline'
 
 function Header() {
   const [search, setSearch] = useRecoilState(searchState);
+  const searchRef = useRef(null)
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
+  const handleSubmitSearch = (e)=>{
+    e.preventDefault()
+    const term = searchRef.current.value
+    setSearch(term)
+  }
 
-  const debouncedHandleSearch = useMemo(
-    () => debounce(handleSearch, 1500),
-    [search]
-  );
-
-  useEffect(() => {
-    return () => {
-      debouncedHandleSearch.cancel();
-    };
-  }, []);
   return (
     <div className=" bg-[#081217] flex justify-between">
-      <div className="p-5 items-center relative flex">
+      <form className="p-5 items-center relative flex">
         <input
           className="text-xs p-2 px-5 outline-none flex-grow"
           type="text"
           placeholder="Search country name"
-          onChange={debouncedHandleSearch}
+          ref={searchRef}
         />
         <SearchIcon className={`w-5 h-5 absolute right-7 ${search ? 'text-gray-700':'text-gray-400'}`}/>
-      </div>
+      <button type="submit" hidden onClick={handleSubmitSearch}>Enter</button>
+      </form>
       <div className="pr-5">
         <Image
           src={Mask}
